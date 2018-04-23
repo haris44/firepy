834993670adf06f2ae86cb3cc7992005b2f63db0
Alexandre <lxndr.bertrand@gmail.com>
2018-04-23 23:17:45 +0200

    add onoff mock

--- src/mock/gpio.js
+++ src/mock/gpio.js
@@ -1,7 +1,9 @@
+import { Gpio } from './onoff.mock'
+
 export const gpio = (nodeEnv) => {
     if (nodeEnv === "production") {
         return require('onoff').Gpio
     } else {
-        return () => { return "coucou" }
+        return () => { return Gpio }
     }
 }
